4fe60eb8eb44d8df09cac7f3876254b5de1fbc65
Madiapps <madiapps@Mac-mini-de-Madiapps.local>
2022-07-25 10:06:44 +0300

    Init structure

---
+++ project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project16
+//
+//  Created by Madiapps on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
