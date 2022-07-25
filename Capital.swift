410540c75137ad97d979ef016cabf2064ed2f6ab
Madiapps <madiapps@Mac-mini-de-Madiapps.local>
2022-07-25 10:47:36 +0300

    Change pinview and add button handler on each pin

---
+++ Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  project16
+//
+//  Created by Madiapps on 25/07/2022.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String?
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
